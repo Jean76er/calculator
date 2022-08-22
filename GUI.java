0a13ffe5449bd1084d9c72ebd88f66eab35e1289
Jean <jctorres4193@gmail.com>
2022-08-22 16:23:00 -0400

    Added all other operations including the use of decimals. Also made GUI cleaner.

--- GUI.java
+++ GUI.java
@@ -1,4 +1,3 @@
-import java.awt.BorderLayout;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -12,23 +11,27 @@ public class GUI implements ActionListener{
     private String display = "";
     private String operator = "";
     private int num;
-    private int numA;
-    private int numB;
+    private double numA;
+    private double numB;
 
     JButton buttonOne = new JButton("1");
     JButton buttonTwo = new JButton("2");
     JButton buttonThree = new JButton("3");
-    JButton buttonDivide = new JButton("/");
+    JButton buttonAdd = new JButton("+");
     JButton buttonFour = new JButton("4");
     JButton buttonFive = new JButton("5");
     JButton buttonSix = new JButton("6");
-    JButton buttonMultiply = new JButton("*");
+    JButton buttonSubtract = new JButton("-");
     JButton buttonSeven = new JButton("7");
     JButton buttonEight = new JButton("8");
     JButton buttonNine = new JButton("9");
-    JButton buttonSubtract = new JButton("-");
+    JButton buttonMultiply = new JButton("x");
     JButton buttonZero = new JButton("0");
     JButton buttonDeciamal = new JButton(".");
+    JButton buttonNegative = new JButton("+/-");
+    JButton buttonDivide = new JButton("/");
+    JButton buttonFill = new JButton("F");
+    JButton buttonPercent = new JButton("%");
     JButton buttonClear = new JButton("C");
     JButton buttonEquals = new JButton("=");
 
@@ -39,97 +42,117 @@ public class GUI implements ActionListener{
         label = new JLabel("");
 
         labelPanel.setBackground(Color.black); //Label size
-        labelPanel.setBounds(150, 0, 300, 100);
+        labelPanel.setBounds(0, 0, 300, 100);
         
         labelPanel.add(label); //Label colors
         labelPanel.setLayout(new GridBagLayout());
         label.setFont(new Font("serif", Font.PLAIN, 35));
-        label.setForeground(Color.yellow);
+        label.setForeground(Color.WHITE);
 
-        buttonPanel.setLayout(new GridLayout(4, 4, 0, 0));
+        buttonPanel.setLayout(new GridLayout(5, 4, 0, 0));
 
         buttonPanel.setBackground(Color.black);
-        buttonPanel.setBounds(150, 100, 300, 300);
+        buttonPanel.setBounds(0, 100, 300, 300);
 
         buttonPanel.add(buttonOne);
         buttonPanel.add(buttonTwo);
         buttonPanel.add(buttonThree);
-        buttonPanel.add(buttonDivide);
+        buttonPanel.add(buttonAdd);
         buttonPanel.add(buttonFour);
         buttonPanel.add(buttonFive);
         buttonPanel.add(buttonSix);
-        buttonPanel.add(buttonMultiply);
+        buttonPanel.add(buttonSubtract);
         buttonPanel.add(buttonSeven);
         buttonPanel.add(buttonEight);
         buttonPanel.add(buttonNine);
-        buttonPanel.add(buttonSubtract);
+        buttonPanel.add(buttonMultiply);
         buttonPanel.add(buttonZero);
         buttonPanel.add(buttonDeciamal);
+        buttonPanel.add(buttonNegative);
+        buttonPanel.add(buttonDivide);
+        buttonPanel.add(buttonFill);
+        buttonPanel.add(buttonPercent);
         buttonPanel.add(buttonClear);
         buttonPanel.add(buttonEquals);
 
-        buttonOne.setBackground(Color.orange);
-        buttonOne.setForeground(Color.black);
+        buttonOne.setBackground(Color.GRAY);
+        buttonOne.setForeground(Color.WHITE);
         buttonOne.setFont(new Font("serif", Font.PLAIN, 35));
 
-        buttonTwo.setBackground(Color.orange);
-        buttonTwo.setForeground(Color.black);
+        buttonTwo.setBackground(Color.GRAY);
+        buttonTwo.setForeground(Color.WHITE);
         buttonTwo.setFont(new Font("serif", Font.PLAIN, 35));
 
-        buttonThree.setBackground(Color.orange);
-        buttonThree.setForeground(Color.black);
+        buttonThree.setBackground(Color.GRAY);
+        buttonThree.setForeground(Color.WHITE);
         buttonThree.setFont(new Font("serif", Font.PLAIN, 35));
 
-        buttonDivide.setBackground(Color.orange);
-        buttonDivide.setForeground(Color.black);
-        buttonDivide.setFont(new Font("serif", Font.PLAIN, 35));
+        buttonAdd.setBackground(Color.ORANGE);
+        buttonAdd.setForeground(Color.WHITE);
+        buttonAdd.setFont(new Font("serif", Font.PLAIN, 35));
 
-        buttonFour.setBackground(Color.orange);
-        buttonFour.setForeground(Color.black);
+        buttonFour.setBackground(Color.GRAY);
+        buttonFour.setForeground(Color.WHITE);
         buttonFour.setFont(new Font("serif", Font.PLAIN, 35));
 
-        buttonFive.setBackground(Color.orange);
-        buttonFive.setForeground(Color.black);
+        buttonFive.setBackground(Color.GRAY);
+        buttonFive.setForeground(Color.WHITE);
         buttonFive.setFont(new Font("serif", Font.PLAIN, 35));
 
-        buttonSix.setBackground(Color.orange);
-        buttonSix.setForeground(Color.black);
+        buttonSix.setBackground(Color.GRAY);
+        buttonSix.setForeground(Color.WHITE);
         buttonSix.setFont(new Font("serif", Font.PLAIN, 35));
 
-        buttonMultiply.setBackground(Color.orange);
-        buttonMultiply.setForeground(Color.black);
-        buttonMultiply.setFont(new Font("serif", Font.PLAIN, 35));
-
-        buttonSeven.setBackground(Color.orange);
-        buttonSeven.setForeground(Color.black);
-        buttonSeven.setFont(new Font("serif", Font.PLAIN, 35));
-
-        buttonEight.setBackground(Color.orange);
-        buttonEight.setForeground(Color.black);
-        buttonEight.setFont(new Font("serif", Font.PLAIN, 35));
-
-        buttonNine.setBackground(Color.orange);
-        buttonNine.setForeground(Color.black);
-        buttonNine.setFont(new Font("serif", Font.PLAIN, 35));
-
-        buttonSubtract.setBackground(Color.orange);
-        buttonSubtract.setForeground(Color.black);
+        buttonSubtract.setBackground(Color.ORANGE);
+        buttonSubtract.setForeground(Color.WHITE);
         buttonSubtract.setFont(new Font("serif", Font.PLAIN, 35));
 
-        buttonZero.setBackground(Color.orange);
-        buttonZero.setForeground(Color.black);
+        buttonSeven.setBackground(Color.GRAY);
+        buttonSeven.setForeground(Color.WHITE);
+        buttonSeven.setFont(new Font("serif", Font.PLAIN, 35));
+
+        buttonEight.setBackground(Color.GRAY);
+        buttonEight.setForeground(Color.WHITE);
+        buttonEight.setFont(new Font("serif", Font.PLAIN, 35));
+
+        buttonNine.setBackground(Color.GRAY);
+        buttonNine.setForeground(Color.WHITE);
+        buttonNine.setFont(new Font("serif", Font.PLAIN, 35));
+
+        buttonMultiply.setBackground(Color.ORANGE);
+        buttonMultiply.setForeground(Color.WHITE);
+        buttonMultiply.setFont(new Font("serif", Font.PLAIN, 35));
+
+        buttonZero.setBackground(Color.GRAY);
+        buttonZero.setForeground(Color.WHITE);
         buttonZero.setFont(new Font("serif", Font.PLAIN, 35));
 
-        buttonDeciamal.setBackground(Color.orange);
-        buttonDeciamal.setForeground(Color.black);
+        buttonDeciamal.setBackground(Color.GRAY);
+        buttonDeciamal.setForeground(Color.WHITE);
         buttonDeciamal.setFont(new Font("serif", Font.PLAIN, 35));
 
-        buttonClear.setBackground(Color.orange);
-        buttonClear.setForeground(Color.black);
+        buttonNegative.setBackground(Color.LIGHT_GRAY);
+        buttonNegative.setForeground(Color.BLACK);
+        buttonNegative.setFont(new Font("serif", Font.PLAIN, 35));
+
+        buttonDivide.setBackground(Color.ORANGE);
+        buttonDivide.setForeground(Color.WHITE);
+        buttonDivide.setFont(new Font("serif", Font.PLAIN, 35));
+
+        buttonFill.setBackground(Color.ORANGE);
+        buttonFill.setForeground(Color.WHITE);
+        buttonFill.setFont(new Font("serif", Font.PLAIN, 35));
+
+        buttonPercent.setBackground(Color.LIGHT_GRAY);
+        buttonPercent.setForeground(Color.BLACK);
+        buttonPercent.setFont(new Font("serif", Font.PLAIN, 35));
+
+        buttonClear.setBackground(Color.LIGHT_GRAY);
+        buttonClear.setForeground(Color.BLACK);
         buttonClear.setFont(new Font("serif", Font.PLAIN, 35));
 
-        buttonEquals.setBackground(Color.orange);
-        buttonEquals.setForeground(Color.black);
+        buttonEquals.setBackground(Color.ORANGE);
+        buttonEquals.setForeground(Color.WHITE);
         buttonEquals.setFont(new Font("serif", Font.PLAIN, 35));
 
         label.setText(display);
@@ -147,11 +170,17 @@ public class GUI implements ActionListener{
         buttonClear.addActionListener(this);
         buttonEquals.addActionListener(this);
         buttonMultiply.addActionListener(this);
+        buttonDivide.addActionListener(this);
+        buttonAdd.addActionListener(this);
+        buttonSubtract.addActionListener(this);
+        buttonDeciamal.addActionListener(this);
+        buttonNegative.addActionListener(this);
+        buttonPercent.addActionListener(this);
 
         frame.add(buttonPanel);
         frame.add(labelPanel);
-        frame.setTitle("First");
-        frame.setSize(600,600);
+        frame.setTitle("Jean's Calculator");
+        frame.setSize(315,440);
         frame.setLayout(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
@@ -204,13 +233,44 @@ public class GUI implements ActionListener{
             this.display = "";
         }
         else if(e.getSource() == buttonMultiply) {
-            numA = Integer.parseInt(display);
+            numA = Double.valueOf(display);
             this.display = "";
+            operator = "m";
+        }
+        else if(e.getSource() == buttonDivide) {
+            numA = Double.valueOf(display);
+            this.display = "";
+            operator = "d";
+        }
+        else if(e.getSource() == buttonAdd) {
+            numA = Double.valueOf(display);
+            this.display = "";
+            operator = "a";
+        }
+        else if(e.getSource() == buttonSubtract) {
+            numA = Double.valueOf(display);
+            this.display = "";
+            operator = "s";
         }
         else if(e.getSource() == buttonEquals) {
-            numB = Integer.parseInt(display);
+            numB = Double.valueOf(display);
             this.display = "";
-            changeString(String.valueOf(Multiply.multiply(numA, numB)));
+            changeString(String.valueOf(Checker.checker(numA, numB, operator)));
+        }
+        else if(e.getSource() == buttonDeciamal) {
+            if(display.contains(".")) {
+                this.display = "";
+            }
+            changeString(".");
+        }
+        else if(e.getSource() == buttonNegative) {
+            if(display.length() > 0) {
+                this.display = "";
+            }
+            changeString("-");
+        }
+        else if(e.getSource() == buttonPercent) {
+            this.display = String.valueOf(Double.valueOf(display) / 100);
         }
 
         label.setText(display);
