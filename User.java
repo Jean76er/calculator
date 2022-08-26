27999d838086f07fe394f90f3612fbf7f5d0ee9a
Jean <jctorres4193@gmail.com>
2022-08-26 15:30:41 -0400

    Made it possible for user to pick between terminal and GUI use

--- User.java
+++ User.java
@@ -8,19 +8,26 @@ public class User {
     static Scanner input = new Scanner(System.in);
 
     public static void start() {
-        while(true) {
-            System.out.println("Welcome to my first attempt at a calculator");
-            System.out.print("Enter first number: ");
-            numA = input.nextInt();
-            System.out.print("Enter second number: ");
-            numB = input.nextInt();
-            System.out.println("Now what would you like to do with the two numbers?");
-            System.out.print("a: Adds first and second number\ns: Subtracts first number by second number\nm: Multiplies first and second number\nd: divides first number by second number\n>>>>>>>>>> ");
-            letter = input.next();
+        System.out.print("Do you want to use the terminal or the GUI?: ");
+        letter = input.next();
+        if(letter.toUpperCase().equals("TERMINAL")) {
+            while(true) {
+                System.out.println("Welcome to my first attempt at a calculator");
+                System.out.print("Enter first number: ");
+                numA = input.nextInt();
+                System.out.print("Enter second number: ");
+                numB = input.nextInt();
+                System.out.println("Now what would you like to do with the two numbers?");
+                System.out.print("a: Adds first and second number\ns: Subtracts first number by second number\nm: Multiplies first and second number\nd: divides first number by second number\n>>>>>>>>>> ");
+                letter = input.next();
 
-            answer = Checker.checker(numA, numB, letter);
+                answer = Checker.checker(numA, numB, letter);
 
-            System.out.println(answer);
+                System.out.println(answer);
+            }
+        }
+        else if(letter.toUpperCase().equals("GUI")); {
+            new GUI();
         }
     }
 }
